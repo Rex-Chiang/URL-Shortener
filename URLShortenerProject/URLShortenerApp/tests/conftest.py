37d0499ae851f5d3bd1d299cba20d0a98ee1caf2
Rex-Chiang <Rex.Chiang@luxshare-ict.com>
2022-01-22 18:10:35 +0800

    Setup testing ENV with Pytest and update unit test for model

---
+++ URLShortenerProject/URLShortenerApp/tests/conftest.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+from django.conf import settings
+
+@pytest.fixture(scope = "session")
+def django_db_setup():
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get('DB_NAME'),
+    }
